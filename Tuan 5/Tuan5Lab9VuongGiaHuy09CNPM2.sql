789ce9fbd04a893cd27870f895ab1d1bd8d7ce1e
Huy49202 <128563579+Huy49202@users.noreply.github.com>
2023-04-19 10:42:40 +0700

    Add files via upload

--- Tuan 5/Tuan5Lab9VuongGiaHuy09CNPM2.sql
+++ Tuan 5/Tuan5Lab9VuongGiaHuy09CNPM2.sql
@@ -67,4 +67,67 @@ BEGIN
     WHERE masp = (SELECT masp FROM inserted)
 END
 
-insert into Xuat values 
+insert into Xuat values ('X06', 'SP05', 'NV01', '2020-12-12', '2')
+
+
+CREATE TRIGGER trig_bai4lab9
+ON Xuat
+AFTER UPDATE
+AS
+BEGIN
+
+    IF (SELECT COUNT(*) FROM inserted) < 2
+    BEGIN
+        DECLARE @old_soluong INT, @new_soluong INT, @masp NVARCHAR(10)
+		SELECT @masp = i.masp, @old_soluong = d.soluongX, @new_soluong = i.soluongX
+        FROM deleted d INNER JOIN inserted i ON d.sohdx = i.sohdx AND d.masp = i.masp
+        IF (@new_soluong <= (SELECT soluong FROM sanpham WHERE masp = @masp))
+        BEGIN
+            UPDATE xuat SET soluongX = @new_soluong WHERE sohdx IN (SELECT sohdx FROM inserted)
+            UPDATE sanpham SET soluong = soluong + @old_soluong - @new_soluong WHERE masp = @masp
+        END
+    END
+END
+
+UPDATE Xuat set ngayxuat = '2020-12-11'
+
+CREATE TRIGGER trig_bai5lab9
+ON Nhap
+AFTER UPDATE
+AS
+BEGIN
+
+    IF (SELECT COUNT(*) FROM inserted) > 1
+    BEGIN
+        RAISERROR('Chỉ được phép cập nhật 1 bản ghi tại một thời điểm', 16, 1)
+        ROLLBACK
+    END
+    
+    DECLARE @masp INT
+    DECLARE @soluongN INT
+    DECLARE @soluong INT
+    
+    SELECT @masp = i.masp, @soluongN = i.soluongN, @soluong = s.soluong
+    FROM inserted i
+    INNER JOIN Sanpham s ON i.masp = s.masp
+    
+    UPDATE Sanpham
+    SET soluong = soluong + (@soluongN - (SELECT soluongN FROM deleted WHERE masp = @masp))
+    WHERE masp = @masp
+END
+UPDATE Xuat set ngayxuat = '2022-12-12'
+
+ALTER TRIGGER trig_bai6lab9
+ON Nhap
+AFTER DELETE
+AS
+
+BEGIN
+    UPDATE Sanpham
+    SET Soluong = Sanpham.Soluong - deleted.soluongN
+    FROM Sanpham
+    JOIN deleted ON Sanpham.Masp = deleted.Masp
+	SELECT count(soluong) as 'So luong cap nhat' from Sanpham
+END
+
+Delete from Nhap where soluongN = 10
